a18167fe2d75d7bed7f7d97a1ab3db1bf8a7c11c
Andrey Kudinov <andrey.kudinov@avaloninnovations.com.au>
2023-02-23 21:25:41 +0400

    Update lists

--- compare.js
+++ compare.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 import path from 'path';
 
 const FIRST_TITLE = 'short-list_06.02.2023.json'
-const SECOND_TITLE = 'short-list_14.02.2023.json'
+const SECOND_TITLE = 'short-list_23.02.2023.json'
 
 const firstList = JSON.parse(fs.readFileSync(path.join('.', 'lists', FIRST_TITLE)))
 const secondList = JSON.parse(fs.readFileSync(path.join('.', 'lists', SECOND_TITLE)))
