0245a18be582407d47f095072798fc570b5dd1b5
Andrey Kudinov <andrey.kudinov@avaloninnovations.com.au>
2023-04-05 23:59:17 +0400

    Update gh list

--- compare.js
+++ compare.js
@@ -1,8 +1,8 @@
 import fs from 'fs'
 import path from 'path';
 
-const FIRST_TITLE = 'short-list_23.02.2023.json'
-const SECOND_TITLE = 'short-list_02.04.2023.json'
+const FIRST_TITLE = 'short-list_02.04.2023.json'
+const SECOND_TITLE = 'short-list_05.04.2023.json'
 
 const firstList = JSON.parse(fs.readFileSync(path.join('.', 'lists', FIRST_TITLE)))
 const secondList = JSON.parse(fs.readFileSync(path.join('.', 'lists', SECOND_TITLE)))
